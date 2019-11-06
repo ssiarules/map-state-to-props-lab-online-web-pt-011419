218a6510f32404359ead6a21b01c4816436b9c20
Johnson Joseph <jhonsonj88@gmail.com>
2019-11-06 01:21:30 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
-import Connect from 'react-redux'
+//import Connect from 'react-redux'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
