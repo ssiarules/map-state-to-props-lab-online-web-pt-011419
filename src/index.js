2528da86d725d313b6caec2d36dcf557382ff46e
Johnson Joseph <jhonsonj88@gmail.com>
2019-11-06 01:20:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'
 import Connect from 'react-redux'
+import { createStore } from 'redux'
 
 import manageUsers from './reducers/manageUsers'
 
